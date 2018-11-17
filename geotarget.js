3c6255f5a09ef19bef3e136fc3ab9ec58fbe9e5c
leniwidi <45127197+leniwidi@users.noreply.github.com>
2018-11-18 01:57:25 +0700

    Create geotarget.js

---
+++ geotarget.js
@@ -0,0 +1,7 @@
+<script>
+var geotargetlyredirect1537052462766 = document.createElement('script');
+geotargetlyredirect1537052462766.setAttribute('type','text/javascript');
+geotargetlyredirect1537052462766.async = 1;
+geotargetlyredirect1537052462766.setAttribute('src', '//geotargetly-1a441.appspot.com/georedirect?id=-LMUSooldVPt_TtPdyd7&refurl='+document.referrer);
+document.getElementsByTagName('head')[0].appendChild(geotargetlyredirect1537052462766);
+</script>
